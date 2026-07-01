4f2fee97b3931fb58cce8655ba1ca85373a958d6
abdullah121205 <abdullahabdullah6189@gmail.com>
2026-07-01 12:13:53 +0530

    Update Add_person.js

--- static/js/Add_person.js
+++ static/js/Add_person.js
@@ -1,1 +1,20 @@
+const addPersonForm = document.getElementById("addPersonForm");
 
+if (addPersonForm) {
+    addPersonForm.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+
+        let name = document.getElementById("name").value;
+        let age = document.getElementById("age").value;
+        let gender = document.getElementById("gender").value;
+        let location = document.getElementById("location").value;
+
+        if (name === "" || age === "" || gender === "" || location === "") {
+            alert("Please fill all fields");
+            return;
+        }
+
+        alert("Person added successfully!");
+    });
+}
