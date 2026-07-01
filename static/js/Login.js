e7f97cd40e6c01a05340897b18da16bdd091444b
abdullah121205 <abdullahabdullah6189@gmail.com>
2026-07-01 12:12:16 +0530

    Update Login.js

--- static/js/Login.js
+++ static/js/Login.js
@@ -1,1 +1,18 @@
+const loginForm = document.getElementById("loginForm");
 
+if (loginForm) {
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+        if (email === "" || password === "") {
+            alert("Please fill all fields");
+            return;
+        }
+
+        alert("Login Successful");
+        window.location.href = "/dashboard";
+    });
+}
