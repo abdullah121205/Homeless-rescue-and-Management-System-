b5afa64bc715111f6b227707bb8aa12e437935b5
abdullah121205 <abdullahabdullah6189@gmail.com>
2026-07-01 12:14:57 +0530

    Update Search.js

--- static/js/Search.js
+++ static/js/Search.js
@@ -1,1 +1,20 @@
+function searchPerson() {
 
+    let input = document.getElementById("searchInput").value.toLowerCase();
+
+    let rows = document.querySelectorAll("#personTable tbody tr");
+
+    rows.forEach(function(row){
+
+        let text = row.innerText.toLowerCase();
+
+        if(text.includes(input)){
+            row.style.display="";
+        }
+        else{
+            row.style.display="none";
+        }
+
+    });
+
+}
