ebec6983540ef141a84248304213bc1e5db3f6f8
abdullah121205 <abdullahabdullah6189@gmail.com>
2026-07-01 12:14:26 +0530

    Update View_person.js

--- static/js/View_person.js
+++ static/js/View_person.js
@@ -1,1 +1,9 @@
+function viewPerson(id) {
+    alert("Viewing details of Person ID: " + id);
+}
 
+function deletePerson(id) {
+    if (confirm("Are you sure you want to delete this record?")) {
+        alert("Person deleted successfully.");
+    }
+}
